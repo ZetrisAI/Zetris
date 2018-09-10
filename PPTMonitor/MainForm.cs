d4184a51eaba03364030c2033a7f66ca2107ef1e
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-09-10 21:59:55 +0200

    Sets and total

--- PPTMonitor/MainForm.cs
+++ PPTMonitor/MainForm.cs
@@ -15,10 +15,14 @@ namespace PPTMonitor {
             InitializeComponent();
         }
 
-        int startingRating, currentRating;
-        static int[] score = new int[4] {0, 0, 0, 0};
-
         static VAMemory PPT = new VAMemory("puyopuyotetris");
+
+        static int startingRating, currentRating;
+
+        static int[] score = new int[4] {0, 0, 0, 0};
+        static int[] sets = new int[4] {0, 0, 0, 0};
+        static int[] total = new int[4] {0, 0, 0, 0};
+        static int maxscore = 2;
 
         private void ScanTimer_Tick(object sender, EventArgs e) {
             currentRating = PPT.ReadInt32(new IntPtr(0x140599FF0));
@@ -26,10 +30,34 @@ namespace PPTMonitor {
             valueRatingDifference.Text = (currentRating - startingRating).ToString();
 
             int scoreAddress = PPT.ReadInt32(new IntPtr(0x14057F048)) + 0x38;
-            value4PScore1.Text = value2PScore1.Text = PPT.ReadInt32(new IntPtr(scoreAddress)).ToString();
-            value4PScore2.Text = value2PScore2.Text = PPT.ReadInt32(new IntPtr(scoreAddress + 0x4)).ToString();
-            value4PScore3.Text                      = PPT.ReadInt32(new IntPtr(scoreAddress + 0x8)).ToString();
-            value4PScore4.Text                      = PPT.ReadInt32(new IntPtr(scoreAddress + 0xC)).ToString();
+
+            maxscore = PPT.ReadInt32(new IntPtr(scoreAddress + 0x10));
+
+            for (int i = 0; i < 4; i++) {
+                int value = PPT.ReadInt32(new IntPtr(scoreAddress) + i * 4);
+                if (value > score[i]) {
+                    total[i]++;
+                    if (value >= maxscore) {
+                        sets[i]++;
+                    }
+                }
+                score[i] = value;
+            }
+
+            value4PScore1.Text = value2PScore1.Text = score[0].ToString();
+            value4PScore2.Text = value2PScore2.Text = score[1].ToString();
+            value4PScore3.Text                      = score[2].ToString();
+            value4PScore4.Text                      = score[3].ToString();
+
+            value4PSets1.Text = value2PSets1.Text = sets[0].ToString();
+            value4PSets2.Text = value2PSets2.Text = sets[1].ToString();
+            value4PSets3.Text                     = sets[2].ToString();
+            value4PSets4.Text                     = sets[3].ToString();
+
+            value4PTotal1.Text = value2PTotal1.Text = total[0].ToString();
+            value4PTotal2.Text = value2PTotal2.Text = total[1].ToString();
+            value4PTotal3.Text                      = total[2].ToString();
+            value4PTotal4.Text                      = total[3].ToString();
 
             int playerAddress = PPT.ReadInt32(new IntPtr(PPT.ReadInt32(new IntPtr(0x140473760)) + 0x20)) + 0x108;
             value4PPlayer1Rating.Text = value2PPlayer1Rating.Text = PPT.ReadInt16(new IntPtr(playerAddress)).ToString();
@@ -53,7 +81,6 @@ namespace PPTMonitor {
 
         private void buttonRehook_Click(object sender, EventArgs e) {
             PPT = new VAMemory("puyopuyotetris");
-            
         }
     }
 }
