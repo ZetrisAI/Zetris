806b49f6767b9a19de499e5306eca338f56985c6
mat1jaczyyy <mat1jaczyyy@gmail.com>
2018-12-17 18:04:40 +0100

    Disallow Form maximization

--- Zetris/MainForm.Designer.cs
+++ Zetris/MainForm.Designer.cs
@@ -434,6 +434,7 @@
             this.ForeColor = System.Drawing.Color.Gainsboro;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Zetris";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_Closing);
