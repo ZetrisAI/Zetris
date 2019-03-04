d301ee69f096a2e4feb296f676a0c556c9cef905
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-04 18:02:42 +0100

    Renderer: Draw Current Piece

--- PPT-TAS/Renderer.cs
+++ PPT-TAS/Renderer.cs
@@ -3,11 +3,56 @@ using System.Windows.Forms;
 
 namespace PPT_TAS {
     public class Renderer {
+        static readonly (int, int)[][][] pieces = new (int, int)[7][][] {
+            new (int, int)[4][] { // S
+                new (int, int)[4] {(1, 0), (2, 0), (0, 1), (1, 1)},
+                new (int, int)[4] {(1, 0), (1, 1), (2, 1), (2, 2)},
+                new (int, int)[4] {(1, 1), (2, 1), (0, 2), (1, 2)},
+                new (int, int)[4] {(0, 0), (0, 1), (1, 1), (1, 2)}
+            },
+            new (int, int)[4][] { // Z
+                new (int, int)[4] {(0, 0), (1, 0), (1, 1), (2, 1)},
+                new (int, int)[4] {(2, 0), (1, 1), (2, 1), (1, 2)},
+                new (int, int)[4] {(0, 1), (1, 1), (1, 2), (2, 2)},
+                new (int, int)[4] {(1, 0), (0, 1), (1, 1), (0, 2)}
+            },
+            new (int, int)[4][] { // L
+                new (int, int)[4] {(2, 0), (0, 1), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (1, 1), (1, 2), (2, 2)},
+                new (int, int)[4] {(0, 1), (1, 1), (2, 1), (0, 2)},
+                new (int, int)[4] {(0, 0), (1, 0), (1, 1), (1, 2)}
+            },
+            new (int, int)[4][] { // J
+                new (int, int)[4] {(0, 0), (0, 1), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (2, 0), (1, 1), (1, 2)},
+                new (int, int)[4] {(0, 1), (1, 1), (2, 1), (2, 2)},
+                new (int, int)[4] {(1, 0), (1, 1), (0, 2), (1, 2)}
+            },
+            new (int, int)[4][] { // T
+                new (int, int)[4] {(1, 0), (0, 1), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (1, 1), (2, 1), (1, 2)},
+                new (int, int)[4] {(0, 1), (1, 1), (2, 1), (1, 2)},
+                new (int, int)[4] {(1, 0), (0, 1), (1, 1), (1, 2)}
+            },
+            new (int, int)[4][] { // O
+                new (int, int)[4] {(1, 0), (2, 0), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (2, 0), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (2, 0), (1, 1), (2, 1)},
+                new (int, int)[4] {(1, 0), (2, 0), (1, 1), (2, 1)}
+            },
+            new (int, int)[4][] { // I
+                new (int, int)[4] {(0, 1), (1, 1), (2, 1), (3, 1)},
+                new (int, int)[4] {(2, 0), (2, 1), (2, 2), (2, 3)},
+                new (int, int)[4] {(0, 2), (1, 2), (2, 2), (3, 2)},
+                new (int, int)[4] {(1, 0), (1, 1), (1, 2), (1, 3)}
+            }
+        };
+        
         PictureBox canvas;
 
         public int[,] board;
         public int[] queue;
-        public int current, y, hold, cleared, bag;
+        public int x, r, current, y, hold, cleared, bag;
 
         readonly SolidBrush[] bg = new SolidBrush[] {
             new SolidBrush(Color.FromArgb(255, Color.FromArgb(0x1A1A1A))),
@@ -39,6 +84,11 @@ namespace PPT_TAS {
                         if (board[i, j] != 255)
                             gfx.DrawImage((Image)Properties.Resources.ResourceManager.GetObject($"Mino_{board[i, j]}"), mino);
                     }
+                }
+
+                foreach ((int, int) offset in pieces[current][r]) {
+                    Rectangle mino = new Rectangle(new Point((x - 1 + offset.Item1) * px.Width, (y - 1 + offset.Item2) * px.Height), px);
+                    gfx.DrawImage((Image)Properties.Resources.ResourceManager.GetObject($"Mino_{current}"), mino);
                 }
 
                 gfx.Flush();
