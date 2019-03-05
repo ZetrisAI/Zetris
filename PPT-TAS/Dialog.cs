a83b8e03663dc8286626d0e8f76c4e63c07b0d5f
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-03-05 16:30:52 +0100

    Dialog: Implement SRS

--- PPT-TAS/Dialog.cs
+++ PPT-TAS/Dialog.cs
@@ -5,6 +5,44 @@ namespace PPT_TAS {
     public partial class Dialog: Form {
         private Renderer gfx;
 
+        private static readonly (int, int)[][][] srs = new (int, int)[4][][] {
+            new (int, int)[2][] { // 0 - 0
+                new (int, int)[5] {( 0,  0), ( 1,  0), ( 1, -1), ( 0,  2), ( 1,  2)},
+                new (int, int)[5] {( 0,  0), (-1,  0), (-1, -1), ( 0,  2), (-1,  2)}
+            },
+            new (int, int)[2][] { // 1 - CW
+                new (int, int)[5] {( 0,  0), (-1,  0), (-1,  1), ( 0, -2), (-1, -2)},
+                new (int, int)[5] {( 0,  0), (-1,  0), (-1,  1), ( 0, -2), (-1, -2)}
+            },
+            new (int, int)[2][] { // 2 - 180
+                new (int, int)[5] {( 0,  0), (-1,  0), (-1, -1), ( 0,  2), (-1,  2)},
+                new (int, int)[5] {( 0,  0), ( 1,  0), ( 1, -1), ( 0,  2), ( 1,  2)}
+            },
+            new (int, int)[2][] { // 3 - CCW
+                new (int, int)[5] {( 0,  0), ( 1,  0), ( 1,  1), ( 0, -2), ( 1, -2)},
+                new (int, int)[5] {( 0,  0), ( 1,  0), ( 1,  1), ( 0, -2), ( 1, -2)}
+            }
+        };
+
+        private static readonly (int, int)[][][] srsI = new (int, int)[4][][] {
+            new (int, int)[2][] { // 0 - 0
+                new (int, int)[5] {( 0,  0), ( 2,  0), (-1,  0), ( 2,  1), (-1, -2)},
+                new (int, int)[5] {( 0,  0), ( 1,  0), (-2,  0), ( 1, -2), (-2,  1)}
+            },
+            new (int, int)[2][] { // 1 - CW
+                new (int, int)[5] {( 0,  0), ( 1,  0), (-2,  0), ( 1, -2), (-2,  1)},
+                new (int, int)[5] {( 0,  0), (-2,  0), ( 1,  0), (-2, -1), ( 1,  2)}
+            },
+            new (int, int)[2][] { // 2 - 180
+                new (int, int)[5] {( 0,  0), (-2,  0), ( 1,  0), (-2, -1), ( 1,  2)},
+                new (int, int)[5] {( 0,  0), (-1,  0), ( 2,  0), (-1,  2), ( 2, -1)}
+            },
+            new (int, int)[2][] { // 3 - CCW
+                new (int, int)[5] {( 0,  0), (-1,  0), ( 2,  0), (-1,  2), ( 2, -1)},
+                new (int, int)[5] {( 0,  0), ( 2,  0), (-1,  0), ( 2,  1), (-1, -2)}
+            }
+        };
+
         private static readonly int initX = 4, initR = 0;
         private int initY;
 
@@ -16,6 +54,67 @@ namespace PPT_TAS {
         private int current, y, hold;
 
         private int offsetX = 0, offsetY = 0;
+
+        private void ApplyOffset(int r) {
+            int c = desiredHold ? ((hold == 255) ? queue[0] : hold) : current;
+
+            desiredX -= offsetX;
+            y -= offsetY;
+
+            if (c == 5) {
+                switch (r) {
+                    case 0:
+                        offsetX = 0;
+                        offsetY = 0;
+                        break;
+
+                    case 1:
+                        offsetX = 0;
+                        offsetY = -1;
+                        break;
+
+                    case 2:
+                        offsetX = 1;
+                        offsetY = -1;
+                        break;
+
+                    case 3:
+                        offsetX = 1;
+                        offsetY = 0;
+                        break;
+                }
+
+            } else if (c == 6) {
+                switch (r) {
+                    case 0:
+                        offsetX = 0;
+                        offsetY = 0;
+                        break;
+
+                    case 1:
+                        offsetX = 1;
+                        offsetY = 0;
+                        break;
+
+                    case 2:
+                        offsetX = 1;
+                        offsetY = 1;
+                        break;
+
+                    case 3:
+                        offsetX = 0;
+                        offsetY = 1;
+                        break;
+                }
+
+            } else {
+                offsetX = 0;
+                offsetY = 0;
+            }
+
+            desiredX += offsetX;
+            y += offsetY;
+        }
 
         public Dialog(int[,] _board, int _current, int _yPos, int _hold, int[] _queue, int _cleared, int _bagIndex) {
             InitializeComponent();
@@ -65,10 +164,10 @@ namespace PPT_TAS {
             gfx.Draw();
         }
 
-        private bool movementCollision(int x) {
+        private bool TestCollision(int x, int y, int r) {
             int c = desiredHold? ((hold == 255)? queue[0] : hold) : current;
 
-            foreach ((int, int) offset in Renderer.pieces[c][desiredR]) {
+            foreach ((int, int) offset in Renderer.pieces[c][r]) {
                 try {
                     if (board[x - 1 + offset.Item1, 24 - y - offset.Item2] != 255) {
                         return false;
@@ -81,8 +180,37 @@ namespace PPT_TAS {
             return true;
         }
 
+        private bool TestRotation(int r, int d) {
+            int c = desiredHold ? ((hold == 255) ? queue[0] : hold) : current;
+
+            ApplyOffset(0);
+
+            if (c == 5) {
+                if (TestCollision(desiredX, y, r)) {
+                    ApplyOffset(r);
+                    return true;
+                }
+
+                ApplyOffset(desiredR);
+                return false;
+            }
+
+            foreach ((int, int) transl in (c == 6)? srsI[r][d] : srs[r][d]) {
+                if (TestCollision(desiredX + transl.Item1, y - transl.Item2, r)) {
+                    desiredX += transl.Item1;
+                    y -= transl.Item2;
+
+                    ApplyOffset(r);
+                    return true;
+                }
+            }
+
+            ApplyOffset(desiredR);
+            return false;
+        }
+
         private void valueX_ValueChanged(object sender, EventArgs e) {
-            if (movementCollision((int)valueX.Value - offsetX)) {
+            if (TestCollision((int)valueX.Value - offsetX, y, desiredR)) {
                 desiredX = (int)valueX.Value;
 
                 gfx.x = desiredX - offsetX;
@@ -104,70 +232,8 @@ namespace PPT_TAS {
                 return;
             }
 
-            if (true /* SRS check? */) {
+            if (TestRotation((int)valueR.Value, (((desiredR - (int)valueR.Value + 4) % 4) & 2) >> 1)) {
                 desiredR = (int)valueR.Value;
-
-                int c = desiredHold? ((hold == 255)? queue[0] : hold) : current;
-
-                if (c == 5) {
-                    desiredX -= offsetX;
-                    y -= offsetY;
-
-                    switch (desiredR) {
-                        case 0:
-                            offsetX = 0;
-                            offsetY = 0;
-                            break;
-
-                        case 1:
-                            offsetX = 0;
-                            offsetY = -1;
-                            break;
-
-                        case 2:
-                            offsetX = 1;
-                            offsetY = -1;
-                            break;
-
-                        case 3:
-                            offsetX = 1;
-                            offsetY = 0;
-                            break;
-                    }
-                
-                } else if (c == 6) {
-                    desiredX -= offsetX;
-                    y -= offsetY;
-
-                    switch (desiredR) {
-                        case 0:
-                            offsetX = 0;
-                            offsetY = 0;
-                            break;
-
-                        case 1:
-                            offsetX = 1;
-                            offsetY = 0;
-                            break;
-
-                        case 2:
-                            offsetX = 1;
-                            offsetY = 1;
-                            break;
-
-                        case 3:
-                            offsetX = 0;
-                            offsetY = 1;
-                            break;
-                    }
-
-                } else {
-                    offsetX = 0;
-                    offsetY = 0;
-                }
-
-                desiredX += offsetX;
-                y += offsetY;
 
                 valueX.Value = desiredX;
 
