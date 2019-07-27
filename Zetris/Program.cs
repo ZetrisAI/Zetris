a1df644c8ef85c523c6f797bfea0d6d3e9cdbd07
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-07-27 02:57:02 +0200

    Program: Don't force JP

--- Zetris/Program.cs
+++ Zetris/Program.cs
@@ -1,23 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Zetris
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+namespace Zetris {
+    static class Program {
         [STAThread]
-        static void Main()
-        {
-            System.Threading.Thread.CurrentThread.CurrentCulture = 
-            System.Threading.Thread.CurrentThread.CurrentUICulture =
-            System.Globalization.CultureInfo.GetCultureInfo("ja-JP");
-
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
