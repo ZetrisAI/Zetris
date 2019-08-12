11bf0eda38891c73d6d73a8390e1823db6701b6e
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-08-12 22:55:05 +0200

    GameHelper: B2B boolean

--- Zetris/GameHelper.cs
+++ Zetris/GameHelper.cs
@@ -849,11 +849,11 @@ namespace Zetris {
             return Math.Max(ret, 0);
         }
 
-        public static int getBackToBack(ProcessMemory Game, int index) {
-            int ret = -1;
+        public static bool getBackToBack(ProcessMemory Game, int index) {
+            byte ret = 0;
             switch (index) {
                 case 0:
-                    ret = Game.ReadInt32(new IntPtr(
+                    ret = Game.ReadByte(new IntPtr(
                         Game.ReadInt32(new IntPtr(
                             Game.ReadByte(new IntPtr(
                                 0x140598A20
@@ -863,7 +863,7 @@ namespace Zetris {
                     break;
 
                 case 1:
-                    ret = Game.ReadInt32(new IntPtr(
+                    ret = Game.ReadByte(new IntPtr(
                         Game.ReadInt32(new IntPtr(
                             Game.ReadByte(new IntPtr(
                                 0x140598A28
@@ -873,7 +873,7 @@ namespace Zetris {
                     break;
 
                 case 2:
-                    ret = Game.ReadInt32(new IntPtr(
+                    ret = Game.ReadByte(new IntPtr(
                         Game.ReadInt32(new IntPtr(
                             Game.ReadByte(new IntPtr(
                                 0x140598A28
@@ -882,7 +882,7 @@ namespace Zetris {
                     ));
                     break;
             }
-            return Math.Max(ret, 0);
+            return ret == 1;
         }
 
         public static int getFrameCount(ProcessMemory Game) => Game.ReadInt32(new IntPtr(
