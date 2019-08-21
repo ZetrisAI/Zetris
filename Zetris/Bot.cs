1c7a3187f00b5ea7d1fda02d9984732362d46326
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-08-21 03:08:29 +0200

    Don't Reset Game in Public version

--- Zetris/Bot.cs
+++ Zetris/Bot.cs
@@ -19,7 +19,9 @@ namespace Zetris {
         static void ResetGame() {
             if (!GameHelper.InSwap() || !Preferences.Auto) return;
 
+#if !PUBLIC
             Process.Start("steam://joinlobby/546050/109775241058543776/76561198802063829");
+#endif
         }
 
         static int gamepadIndex = 4;
