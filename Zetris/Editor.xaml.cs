e049cf1375268f1b857e4284c7d1b514e5cf1018
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-09-06 20:05:32 +0200

    Editor: Use StyleViewer; Implement New, Duplicate and Delete

--- Zetris/Editor.xaml.cs
+++ Zetris/Editor.xaml.cs
@@ -29,28 +29,75 @@ namespace Zetris {
             FreezeEvents = false;
 
             foreach (Style style in Preferences.Styles)
-                StyleList.Items.Add(style);
+                StyleList.Items.Add(new StyleViewer(style, this));
 
             StyleList.SelectedIndex = Preferences.StyleIndex;
 
             switch (CultureInfo.CurrentCulture.TwoLetterISOLanguageName) {
                 case "ko":
-
+                    Title = "";
                     break;
                     
                 case "ja":
-
+                    Title = "";
                     break;
                     
                 default:
-
+                    Title = "Style Editor";
                     break;
             }
         }
 
         void StyleListChanged(object sender, SelectionChangedEventArgs e) {
+            if (StyleList.SelectedIndex == -1) return;
+
             for (int i = 0; i < 17; i++)
                 ((Dial)Layout.Children[i]).RawValue = Preferences.Styles[StyleList.SelectedIndex].GetParameter(i);
+        }
+
+        void Insert(int index, Style style) {
+            Preferences.Styles.Insert(index, style);
+            Preferences.Save();
+
+            StyleList.Items.Insert(index, new StyleViewer(style, this));
+            StyleList.SelectedIndex = index;
+        }
+
+        public void New(StyleViewer item) =>
+            Insert(StyleList.Items.IndexOf(item), new Style("New Style"));
+
+        public void Duplicate(StyleViewer item) {
+            int index = StyleList.Items.IndexOf(item);
+            Insert(index, Preferences.Styles[index].Clone());
+        }
+
+        public void Delete(StyleViewer item) {
+            if (Preferences.Styles.Count <= 1) return;
+
+            int index = StyleList.Items.IndexOf(item);
+
+            Preferences.Styles.RemoveAt(index);
+            if (Preferences.StyleIndex >= index) Preferences.StyleIndex--;
+            Preferences.Save();
+            
+            StyleList.Items.RemoveAt(index);
+            StyleList.SelectedIndex = Math.Min(index, StyleList.Items.Count - 1);
+        }
+
+        public void Import(StyleViewer item) {
+            int index = StyleList.Items.IndexOf(item);
+
+        }
+
+        public void Export(StyleViewer item) {
+            int index = StyleList.Items.IndexOf(item);
+
+        }
+
+        void NewEmpty(object sender, RoutedEventArgs e) => Insert(Preferences.Styles.Count, new Style("New Style"));
+
+        void ImportEmpty(object sender, RoutedEventArgs e) {
+            
         }
 
         void ParameterChanged(Dial sender, double NewValue) {
